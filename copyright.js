1a3d9fae68744071a62d8f272612235367d64437
Dito <56707407+sianaksusu@users.noreply.github.com>
2019-10-18 18:53:09 +0700

    Add files via upload

---
+++ copyright.js
@@ -0,0 +1,2 @@
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://warunggaptek.blogspot.com/") {
+        window.location.href = "https://warunggaptek.blogspot.com/"; }});
