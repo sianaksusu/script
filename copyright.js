20e7b7ce0469b2008ea3e6ba5173d18236fc679f
Dito <56707407+sianaksusu@users.noreply.github.com>
2019-10-19 12:26:21 +0700

    Delete copyright.js

--- copyright.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function () { if ($("#change,.change").attr("href") != "https://warunggaptek.blogspot.com/") {
-        window.location.href = "https://warunggaptek.blogspot.com/"; }});
