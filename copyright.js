c785cac81f42729e71a1bce91f28531325759a91
Dito <56707407+sianaksusu@users.noreply.github.com>
2019-10-19 10:31:32 +0700

    Update copyright.js

--- copyright.js
+++ copyright.js
@@ -1,2 +1,2 @@
-$(document).ready(function () { if ($("#change,.change").attr("href") != "https://warunggaptek.blogspot.com/") {
+$(document).ready(function () { if ($("#HTML4,.HTML4").attr("href") != "https://warunggaptek.blogspot.com/") {
         window.location.href = "https://warunggaptek.blogspot.com/"; }});
